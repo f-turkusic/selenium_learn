b9828171b60eeb30f933caa88ef3bd31b6143267
fturkusic <farukturkusic@gmail.com>
2024-09-18 23:25:30 +0200

    add explination on FindElement test

--- src/Tests/FindElementByText.java
+++ src/Tests/FindElementByText.java
@@ -25,15 +25,14 @@ public class FindElementByText {
         options.addArguments("--disable-gpu"); // Optional, to avoid some issues
         options.addArguments("--window-size=1920,1080"); // Set a window size if needed
 
-        driver=new ChromeDriver(options);
+        driver = new ChromeDriver(options);
 
         // Declare and initialise a fluent wait for WebDriver
         FluentWait<WebDriver> wait = new FluentWait<>(driver);
         wait.withTimeout(Duration.ofSeconds(5));
         wait.pollingEvery(Duration.ofMillis(250));
         // Specify what exceptions to ignore (NoSuchElementException or any other specific exceptions)
-        wait.ignoring(NoSuchElementException.class)
-                .ignoring(StaleElementReferenceException.class); // You can add multiple exceptions if needed
+        wait.ignoring(NoSuchElementException.class).ignoring(StaleElementReferenceException.class); // You can add multiple exceptions if needed
 
 
     }
@@ -43,8 +42,14 @@ public class FindElementByText {
 
         driver.manage().window().maximize();
         driver.get("https://www.browserstack.com/");
+        By.xpath("//*[text()='Get started free']");
+
+        // The XPath expression "//*[text()='Get started free']" is used to find an element based on its text content:
+        ////* selects all elements in the DOM (Document Object Model).
+        //[text()='Get started free'] filters those elements to find the one whose exact text content is "Get started free".
+        //This expression will match any element (like a button, a tag, div, etc.) on the page that contains the text "Get started free".
         element = driver.findElement(By.xpath("//*[text()='Get started free']"));
-        System.out.println("Element with text(): " + element.getText() );
+        System.out.println("Element with text(): " + element.getText());
 
 
     }
